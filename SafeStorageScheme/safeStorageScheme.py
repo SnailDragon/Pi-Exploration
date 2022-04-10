8b0dd7186f9b5ca0026a05f4d36cca7d4800dedc
Tyler Nielsen <tylernielsenn@gmail.com>
2022-04-10 15:04:16 -0600

    decreased write time

--- SafeStorageScheme/safeStorageScheme.py
+++ SafeStorageScheme/safeStorageScheme.py
@@ -2,42 +2,48 @@
 Collects data and writes to a csv, 
 writes after a determined cycle count to avoid slowdown from writing too often
 """
-#import RPi.GPIO as gp
+import RPi.GPIO as gp
 import time
 from random import random
 
-#gp.setmode(BCM)
+gp.setmode(gp.BCM)
+
 
 ID = time.strftime("%H%M%S", time.localtime()) + str(int(random() * 100))
 print(ID)
-STORING_GAP = 100 # how many cycles to wait until you take the time to save
+STORING_GAP = 10 # how many cycles to wait until you take the time to save
 START_TIME = time.time()
+file = open(f"Results{ID}.csv", 'a')
+
 it = 0
 data = []
-timeStamps = []
 
 try:
-    #main loop
-    while True:
-        #collecting data
-        data.append(random())
-        timeStamps.append(time.time())
+	#main loop
+	while True:
+		#collecting data
+		dataIn = "" #string to save as a line
+		
+		dataIn += str(time.time()) + ", "
 
-        #saving data
-        it += 1
-        if(it % STORING_GAP == 0):
-            start = time.time()
-            with open(f"Results{ID}.csv", 'a') as file:
-                for i,t in zip(data,timeStamps) : file.write(str(t) + ", " + str(i) + ", \n")
-            #clear stored values
-            data.clear()
-            timeStamps.clear()
-            print("Write time: " + str(time.time() - start))
-        
+		dataIn += str(random()) + ", " 
+	
+		data.append(dataIn + "\n")
 
-        time.sleep(0.013)
+		#saving data
+		it += 1
+		if(it % STORING_GAP == 0):
+			start = time.time()
+			file.writelines(data)
+			#clear stored values
+			data.clear()
+			
+			print("Write time: " + str(time.time() - start))
+
+		time.sleep(0.013)
 
 #clean up for debugging 
 except KeyboardInterrupt:
-    print("--Interrupted--")
-    #gp.cleanup()
+	print("--Interrupted--")
+	gp.cleanup()
+
